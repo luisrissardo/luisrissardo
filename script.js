0f27191720a0c52404bd42f1c9b0aa1eff857460
luisrissardo <134444834+luisrissardo@users.noreply.github.com>
2023-10-04 15:04:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var menuButton = document.getElementById("menu-button");
+var menuList = document.getElementById("menu-list");
+
+menuButton.addEventListener("click", function() {
+  menuList.classList.toggle("open");
+});
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
